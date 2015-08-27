3279c15eb9c8d66ccad6bc9ee374d314d0408bca
avniahuja2000 <pinkpiano2000@gmail.com>
2015-08-27 13:05:27 +0000

    link push

---
+++ models/model.rb
@@ -0,0 +1,4 @@
+# Hash
+
+# <div><iframe width="500px" title="Public Recycling Bins" height="425px" src="https://data.cityofnewyork.us/w/sxx4-xhzg/25te-f2tw?cur=BZhoCkzw311&from=root" frameborder="0"scrolling="no"><a href="https://data.cityofnewyork.us/Environment/Public-Recycling-Bins/sxx4-xhzg" title="Public Recycling Bins" target="_blank">Public Recycling Bins</a></iframe><p><a href="http://www.socrata.com/" target="_blank">Powered by Socrata</a></p></div>
+
